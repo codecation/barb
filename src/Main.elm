472ecbcdf4f9513be77f7919d1aa31a7ad8232cf
Edwin Morris <edwin@rapgenius.com>
2016-10-27 15:41:14 -0700

    initial commit, repo from yeoman

---
+++ src/Main.elm
@@ -0,0 +1,66 @@
+
+module Main exposing (..)
+
+-- Elm Core
+import Html exposing (..)
+import Html.App
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+-- MODEL
+
+
+type alias Model =
+  {
+  }
+
+
+init : (Model, Cmd Msg)
+init =
+  ( {
+    }
+  , Cmd.none
+  )
+
+
+-- UPDATE
+
+
+type Msg
+  = NoOp
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NoOp ->
+      (model, Cmd.none)
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div
+  []
+  [text "Hello world"]
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+main : Program Never
+main =
+  Html.App.program
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = subscriptions
+    }
