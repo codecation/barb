a10c7f73723b1a40599cdc4faabce87b67052e88
Ben Orenstein <ben.orenstein@gmail.com>
2016-10-27 16:22:11 -0700

    Wire up button

--- src/Main.elm
+++ src/Main.elm
@@ -64,7 +64,7 @@ view model =
             []
         , div [ class "controls" ]
             [ div [ class "controls-start" ]
-                [ button [] [ text "Start" ] ]
+                [ button [ Html.Events.onClick Start ] [ text "Start" ] ]
             ]
         ]
 
