d6fc61674af16cd29893b0849fd4e4fbe492b4e7
Edwin Morris <edwin@rapgenius.com>
2016-10-28 13:28:05 -0700

    correctly name functions
    the generated and uploaded images were in incorrectly named functions,
    switched

--- static/index.js
+++ static/index.js
@@ -8,11 +8,6 @@ var Elm = require('../src/Main');
 var app = Elm.Main.embed(document.getElementById('main'));
 
 var getUploadedImageData = function() {
-  var canvas = document.getElementsByTagName('canvas')[0];
-  return canvas.getContext('2d').getImageData(0, 0, 300, 300).data;
-};
-
-var getGeneratedImageData = function() {
   var canvas = document.createElement('canvas');
   var context = canvas.getContext('2d');
   var img = document.getElementsByClassName('images-original_image_container-image')[0];
@@ -21,6 +16,11 @@ var getGeneratedImageData = function() {
   context.drawImage(img, 0, 0);
   return context.getImageData(0, 0, img.width, img.height).data;
 }
+
+var getGeneratedImageData = function() {
+  var canvas = document.getElementsByTagName('canvas')[0];
+  return canvas.getContext('2d').getImageData(0, 0, 300, 300).data;
+};
 
 app.ports.requestImageDetails.subscribe(function(_) {
   var uploadedImageData = Array.from(getUploadedImageData());
